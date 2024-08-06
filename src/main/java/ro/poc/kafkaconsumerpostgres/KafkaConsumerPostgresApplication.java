989989d9c8ff3270f074d1fc77eaa2eb4a0438cf
oane <ionut.popa@esolutions.ro>
2024-08-06 16:19:11 +0300

    project setup

---
+++ src/main/java/ro/poc/kafkaconsumerpostgres/KafkaConsumerPostgresApplication.java
@@ -0,0 +1,13 @@
+package ro.poc.kafkaconsumerpostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerPostgresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaConsumerPostgresApplication.class, args);
+    }
+
+}
